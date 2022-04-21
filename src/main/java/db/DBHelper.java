1f8c321eddf6bb5571f2a07ae03d4f89ad479a99
a.suhorukova <a.sukhorukova@itfbgroup.ru>
2022-04-21 19:57:18 +0300

    fix задание 7

--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -6,74 +6,60 @@ import java.util.HashMap;
 
 public class DBHelper {
 
-    static  String  url = "jdbc:mysql://db4free.net:3306/testdatabase9290";
-    static  String  userName = "testdatabase9290";
-    static  String  password = "123qweASD";
+    static   String  url = "jdbc:mysql://db4free.net:3306/testdatabase9290";
+    static   String  userName = "testdatabase9290";
+    static   String  password = "123qweASD";
 
     static Connection con = null;
-    static Statement st = null;
+    static Statement st =null;
 
     public static Connection init(){
-
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection(url,userName,password);
+            con = DriverManager.getConnection(url, userName, password);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
-
         return con;
     }
 
-    public static ArrayList<HashMap<String, Object>> getSqlResult(final String sql1, String sql){
+    public static ArrayList<HashMap<String, Object>> getSqlResult(final String sql) {
 
         ResultSet rs = null;
-
         init();
-
         ArrayList<HashMap<String, Object>> sqlResult = new ArrayList<>();
         HashMap<String, Object> map = null;
-
         try {
             st = con.createStatement();
-            int count = st.executeUpdate(sql1);
-            System.out.println("Количество добавленных строк = " + count);
-            if (count > 0) {
-                System.out.println("Запись добавлена!");
-            }
-
             rs = st.executeQuery(sql);
-
-
             ResultSetMetaData rsmd = rs.getMetaData();
 
-            while (rs.next()){
+            while (rs.next()) {
                 map = new HashMap<String, Object>(rsmd.getColumnCount());
-                for (int i=1; i<=rsmd.getColumnCount(); i++){
-                    if (rs.getObject(i)==null){
-                        map.put(rsmd.getColumnName(i),"");
-                    }else{
+                for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+                    if (rs.getObject(i) == null) {
+                        map.put(rsmd.getColumnName(i), "");
+                    } else {
                         map.put(rsmd.getColumnName(i), rs.getObject(i));
                     }
                 }
-
                 sqlResult.add(map);
             }
-
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }finally {
+        } finally {
             try {
                 rs.close();
-                if(rs!=null){
+                if (rs != null) {
                     rs.close();
                 }
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
             }
+
             try {
                 st.close();
-                if(st!=null){
+                if (st != null) {
                     st.close();
                 }
             } catch (SQLException e) {
@@ -82,16 +68,54 @@ public class DBHelper {
 
             try {
                 con.close();
-                if(con!=null){
+                if (con != null) {
                     con.close();
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
-
         return sqlResult;
+    }
+    public static String getValueLastRow(String sql, String colName) {
+        String value = getSqlResult(sql).get(0).get(colName).toString();
+
+        return value;
+    }
+
+    public static int getInsert(String sql) {
+
+        init();
+        int row = 0;
+
+        try {
+            st = con.createStatement();
+            row = st.executeUpdate(sql);
 
 
+        } catch (
+                SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                st.close();
+                if (st != null) {
+                    st.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                con.close();
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return row;
     }
 }
+
--- src/test/java/dbTests/DBGetTestDZ7.java
+++ src/test/java/dbTests/DBGetTestDZ7.java
@@ -1,18 +1,44 @@
+
+////
+////import db.DBHelper;
+////import org.testng.annotations.Test;
+////
+////public class DBGetTestDZ7 {
+////
+////    @Test
+////    public void startTest() {
+////        String sql = "SELECT * FROM REGISTRATION";
+////        DBHelper.getSqlResult(sql).forEach(s -> System.out.println(s));
+////        System.out.println(DBHelper.getValueFirstRow(sql, "first"));
+////
+////        String sql2 = "INSERT REGISTRATION(id, last, first, age) VALUES (20, 'lastname6', 'name6', 31)";
+////        DBHelper.getSqlResult(sql).forEach(s -> System.out.println(s));
+////        System.out.println("Добавлено строк: " + DBHelper.getInsertSQL(sql2));
+////
+////        String sql3 = "INSERT REGISTRATION(id, last, first, age) VALUES (21, 'lastname7', 'name7', 19)";
+////        DBHelper.getSqlResult(sql).forEach(s -> System.out.println(s));
+////        System.out.println("Добавлено строк: " + DBHelper.getInsertSQL(sql2) + DBHelper.getInsertSQL(sql3));
+////
+////    }
+////}
 package dbTests;
 
 import db.DBHelper;
 import org.testng.annotations.Test;
 
+import java.util.Scanner;
+
 public class DBGetTestDZ7 {
 
     @Test
-    public void startTest() {
+    public static void startTest() {
+        String sql = "SELECT id FROM REGISTRATION ORDER BY id DESC LIMIT 1";
+        DBHelper.getSqlResult(sql).forEach(s->System.out.println(s));
 
-        String sql1 = "INSERT INTO REGISTRATION (id,last,first,age) " +
-                "VALUES (5, 'username123456', 'password123456', '123456')";
+        String sql2 = "INSERT INTO REGISTRATION(id, last, first, age) SELECT coalesce (MAX(id),0) + 1, 'lastname6', 'name6', 45 FROM REGISTRATION";
+        System.out.println("Добавлено строк: " + DBHelper.getInsert(sql2));
+        DBHelper.getSqlResult(sql).forEach(s->System.out.println(s));
 
-        String sql = "SELECT * FROM REGISTRATION";
-
-        DBHelper.getSqlResult(sql1, sql).forEach(s-> System.out.println(s));
     }
+
 }
