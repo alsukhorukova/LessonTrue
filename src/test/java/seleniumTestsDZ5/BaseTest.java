5ddd807975c3273a5d06d99c013eaa8b388a9a8c
a.suhorukova <a.sukhorukova@itfbgroup.ru>
2022-04-25 11:36:46 +0300

    fix задание 6

--- src/test/java/seleniumTestsDZ5/BaseTest.java
+++ src/test/java/seleniumTestsDZ5/BaseTest.java
@@ -1,6 +1,7 @@
 package seleniumTestsDZ5;
 
 import io.qameta.allure.Attachment;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.AbstractDriverOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
@@ -19,6 +21,7 @@ import utils.PropertyLoader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.Driver;
+import java.util.Set;
 
 public class BaseTest {
     WebDriver driver;
@@ -28,12 +31,28 @@ public class BaseTest {
     public static String setStendKey = System.setProperty("StendKey", "stend1");
     public static String stendKey = System.getProperty("StendKey");
     public static String stendUrl = PropertyLoader.loadProperty(stendKey);
+    public static String browserNameKey = System.getProperty("BrowserName", "browserName1");
+    public static String browserName = PropertyLoader.loadProperty(browserNameKey);
 
  @BeforeTest
     public void beforeT() {
-         System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriver.exe");
-         FirefoxOptions of = new FirefoxOptions();
+     WebDriver driver;
 
+         AbstractDriverOptions ad = null;
+
+         if (browserName.equals("chrome")) {
+             c.logToReport("Chrome");
+             System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver.exe");
+             ad.setCapability("browserName","chrome");
+             ad = new ChromeOptions();
+         }
+
+         else if (browserName.equals("firefox")) {
+             c.logToReport("Firefox");
+             System.setProperty("webdriver.gecko.driver", "./src/main/resources/geckodriver.exe");
+             ad.setCapability("browserName","firefox");
+             ad = new FirefoxOptions();
+         }
 
          URL hub = null;
          try {
@@ -42,7 +61,7 @@ public class BaseTest {
              e.printStackTrace();
          }
 
-         driver = new RemoteWebDriver(hub, of);
+         driver = new RemoteWebDriver(hub, ad);
 
      }
 
